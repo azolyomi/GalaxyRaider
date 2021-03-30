a72eee7ead5184c4b9f405e784bf550035c93169
theurul <andras.zolyomi@gmail.com>
2021-03-30 15:06:36 -0600

    updated changereqsheet to allow user to change all

--- raiding_functions/changereqsheet.js
+++ raiding_functions/changereqsheet.js
@@ -8,15 +8,25 @@ function changereqsheet(msg, args) {
     if (!CONFIG.SystemConfig.servers[msg.guildID]) return "Server is not configurated yet. Type \`.config\` to configurate it.";
 
     let dungeon = args.shift();
-    if (!dungeon || !RAIDCONSTANTS.acceptableRunTypes.includes(dungeon)) return `Dungeon type must be one of ${RAIDCONSTANTS.acceptableRunTypes.join(", ")}`;
-    let index = RAIDCONSTANTS.acceptableRunTypes.indexOf(dungeon);
+    if (!dungeon || (!RAIDCONSTANTS.acceptableRunTypes.includes(dungeon) && dungeon != "all")) return `Dungeon type must be one of ${RAIDCONSTANTS.acceptableRunTypes.join(", ")}`;
     let url = args.shift();
     if (!isImageURL(url)) return "Image URL must be a valid image and a valid URL. To use a discord image, right click it and hit 'Copy Link'.";
+    if (dungeon == "all") {
+        CONFIG.SystemConfig.servers[msg.guildID].defaultreqsheets.forEach(slot => {
+            slot = url
+        })
+        CONFIG.updateConfig(msg.guildID);
+        return ("All requirement sheets updated.");
+    }
+    else {
+        let index = RAIDCONSTANTS.acceptableRunTypes.indexOf(dungeon);
+        CONFIG.SystemConfig.servers[msg.guildID].defaultreqsheets[index] = url;
+        CONFIG.updateConfig(msg.guildID);
+        return (`Requirement sheet for \`${dungeon}\` updated.`);
+    }
+    
 
-    CONFIG.SystemConfig.servers[msg.guildID].defaultreqsheets[index] = url;
-    CONFIG.updateConfig(msg.guildID);
-
-    return ("Req sheet changed.");
+    
 }
 
 exports.changereqsheet = changereqsheet;
@@ -27,8 +37,8 @@ Used to alter the default req sheet posted in the AFK check for a specific AFK c
 
 **Usage**: \`.changereqsheet <dungeonType> <newReqSheetURL>\`
 
-**<dungeonType>**: The type of dungeon, one of \`[${RAIDCONSTANTS.acceptableRunTypes.join(", ")}]\`
+**<dungeonType>**: The type of dungeon, one of \`[all, ${RAIDCONSTANTS.acceptableRunTypes.join(", ")}]\`
 
 **<newReqSheetURL>** The URL for the new req sheet. Must be a valid image URL. To use a Discord image, right click it and hit 'Copy Link'.
 
-**Example**: \`.changereqsheet void https://xyz.com/abc.png\` -> will set the req sheet for void runs to this URL. (Note: this is an invalid URL and will not work.)`
+**Example**: \`.changereqsheet all https://xyz.com/abc.png\` -> will set all req sheets to this URL. (Note: this is an invalid URL and will not work.)`
