37384d64fb7dbe94f1b941474c077aacbe05dc52
theurul <andras.zolyomi@gmail.com>
2021-04-19 13:51:01 -0600

    STD Custom Command

--- app.js
+++ app.js
@@ -1087,7 +1087,74 @@ CONSTANTS.bot.registerCommand("confighelp", function(msg, args) {
 })
 
 
+// SEPARATOR: <CUSTOM>
 
+
+// Custom guild command for Space Travel Dungeons ($20 Patreon Subscription)
+
+CONSTANTS.bot.registerCommand("fuck", function(msg, args) {
+    if (!msg.guildID || msg.guildID != "522815906376843274") return;
+    else if (!(msg.mentions.length > 0)) return "Mention a user.";
+
+    if (msg.member.id == msg.mentions[0].id) return {
+        embed: {
+            title: "Ruh roh!",
+            description: "Masturbation is illegal in Indonesia! Pick someone else to fuck.",
+            color: 0xff0000
+        }
+    }
+    let rand = Math.random() * 100 + 1;
+
+    if (rand < 50) {
+        return {
+            embed: {
+                title: "What's going on here?!",
+                description: 
+                `Pictured: 
+                **Left**: ${msg.member.mention},
+                **Right**: ${msg.mentions[0].mention}`,
+                image: {
+                    url: "https://i.imgur.com/okrfYto.gif",
+                },
+                color: 0xffb6c1
+            }
+        }
+    }
+    else {
+        CONSTANTS.bot.createMessage(msg.channel.id, {
+            embed: {
+                title: "Incoming!",
+                color: 0xffb6c1
+            }
+        })
+        CONSTANTS.bot.createMessage(msg.channel.id, {
+            embed: {
+                title: "3...",
+                color: 0xffb6c1
+            }
+        })
+        CONSTANTS.bot.createMessage(msg.channel.id, {
+            embed: {
+                title: "2...",
+                color: 0xffb6c1
+            }
+        })
+        CONSTANTS.bot.createMessage(msg.channel.id, {
+            embed: {
+                title: "1...",
+                color: 0xffb6c1
+            }
+        })
+        return {
+            embed: {
+                title: "Yahtzee!",
+                description: `${msg.member.mention} successfully fucked ${msg.mentions[0].mention}`,
+                color: 0x00ff00
+            }
+        }
+    }
+
+})
 
 
 
@@ -1287,6 +1354,16 @@ CONSTANTS.bot.on("ready", () => {
         name: ".patreon | .instructions"
     });
 })
+
+
+
+
+
+
+
+
+
+
 
 
 
