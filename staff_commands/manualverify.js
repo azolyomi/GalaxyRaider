c3a239eadfa51723c12a5f559296566bd2e313d7
theurul <andras.zolyomi@gmail.com>
2021-07-08 21:12:55 -0600

    Mandatory nickname for ManualVerify, add IGN to log embed for ManualVerify

--- staff_commands/manualverify.js
+++ staff_commands/manualverify.js
@@ -5,26 +5,29 @@ async function manualVerify(msg, args) {
     if (!CONFIG.SystemConfig.servers[msg.guildID]) return "You first have to configurate the server. Type \`.instructions\` for help.";
     else if (!msg.member.roles.some(role => CONFIG.SystemConfig.servers[msg.guildID].securityroles.includes(role))) return "You need to have a security role that is configured with the bot for that to work.";
     else if (!CONFIG.SystemConfig.servers[msg.guildID].nonstaff.memberaccess.length > 0) return `You first have to configurate a member role with \`.accessrole member <@role>\`.`;
+    else if (!(msg.mentions.length > 0)) return `Mention a user to manually verify!`;
+    args = args.filter(arg => !arg.includes(msg.mentions[0].id));
+    if (!(args.length > 0)) return `Specify the nickname of the user.`;
     try {
-        let nick;
-        if (args[1]) nick = args[1]; 
+        console.log(args);
         let member = await CONSTANTS.bot.getRESTGuildMember(msg.guildID, msg.mentions[0].id).catch(() => {
             CONSTANTS.bot.createMessage(msg.channel.id, `That mention doesn't match a member in your server.`);
             return undefined;
         })
+        let nick = args.shift();
         if (member) {
             await CONFIG.SystemConfig.servers[msg.guildID].nonstaff.memberaccess.forEach(roleid => {
                 member.addRole(roleid);
             })
-            if (nick) await member.edit({
+            await member.edit({
                 nick: nick
-            });
+            })
             try {
                 CONSTANTS.bot.deleteMessage(msg.channel.id, msg.id);
                 CONSTANTS.bot.createMessage(CONFIG.SystemConfig.servers[msg.guildID].logchannel, {
                     embed: {
                         title: "Manual Verification",
-                        description: `${msg.member.mention} just manually verified ${member.mention}. \nRoles added: ${CONFIG.SystemConfig.servers[msg.guildID].nonstaff.memberaccess.map(roleid => `<@&${roleid}>`).join(", ")}`,
+                        description: `${msg.member.mention} just manually verified ${member.mention}. \nIGN: \`${nick}\`\nRoles added: ${CONFIG.SystemConfig.servers[msg.guildID].nonstaff.memberaccess.map(roleid => `<@&${roleid}>`).join(", ")}`,
                         color: 0x6e99d7
                     }
                 })
